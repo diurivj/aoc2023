7d672f166a3c683ed9c024b4e8cfa5992a2154a5
Diego Vazquez <diurivj@gmail.com>
2023-12-03 23:15:10 -0600

    Day 1 second part

--- day1/index.ts
+++ day1/index.ts
@@ -1,22 +1,26 @@
 import { input } from "./input";
 
-// const test_input = `
-// 1abc2
-// pqr3stu8vwx
-// a1b2c3d4e5f
-// treb7uchet
-// `;
+const numberMap: Record<string, number> = {
+	zero: 0,
+	one: 1,
+	two: 2,
+	three: 3,
+	four: 4,
+	five: 5,
+	six: 6,
+	seven: 7,
+	eight: 8,
+	nine: 9,
+};
 
-function main() {
+function part1() {
 	const lines: Array<string> = input.split("\n").filter(Boolean);
 	const numbers: Array<number> = [];
 
 	for (const line of lines) {
 		const numbersInLine: Array<number> = line.match(/[0-9]/g)?.map(Number) ?? [];
-
 		const firstNumber = numbersInLine[0];
 		const lastNumber = numbersInLine[numbersInLine.length - 1];
-		console.log(firstNumber, lastNumber);
 		const number = Number(`${firstNumber}${lastNumber}`);
 		numbers.push(number);
 	}
@@ -27,4 +31,31 @@ function main() {
 	return sum;
 }
 
-main();
+function part2() {
+	const lines: Array<string> = input.split("\n").filter(Boolean);
+	const numbers: Array<number> = [];
+
+	for (const line of lines) {
+		const numbersInLine = Array.from(
+			line.matchAll(/(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))/g),
+		)
+			.flat()
+			.filter(Boolean);
+		const firstNumber =
+			numbersInLine[0].length > 1 ? numberMap[numbersInLine[0]] : numbersInLine[0];
+		const lastNumber =
+			numbersInLine[numbersInLine.length - 1].length > 1
+				? numberMap[numbersInLine[numbersInLine.length - 1]]
+				: numbersInLine[numbersInLine.length - 1];
+
+		const num = `${firstNumber}${lastNumber}`;
+		numbers.push(Number(num));
+	}
+
+	const sum = numbers.reduce((a, b) => a + b, 0);
+	console.log(sum);
+
+	return sum;
+}
+
+part2();
