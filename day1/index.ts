8163b9a4350e115c1d48a190198542535d90115b
Diego Vazquez <diurivj@gmail.com>
2023-12-03 22:50:53 -0600

    fixing biome formatting and first part of day1

--- day1/index.ts
+++ day1/index.ts
@@ -1,25 +1,30 @@
-const test_input = `
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-s4ntacl4u5
-`;
+import { input } from "./input";
+
+// const test_input = `
+// 1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet
+// `;
 
 function main() {
-	const lines: Array<string> = test_input.split("\n").filter(Boolean);
+	const lines: Array<string> = input.split("\n").filter(Boolean);
 	const numbers: Array<number> = [];
 
 	for (const line of lines) {
-		const numbersInLine: Array<number> = line.match(/\d+/g)?.map(Number) ?? [];
+		const numbersInLine: Array<number> = line.match(/[0-9]/g)?.map(Number) ?? [];
+
 		const firstNumber = numbersInLine[0];
-		const lastNumber = numbersInLine.at(-1);
+		const lastNumber = numbersInLine[numbersInLine.length - 1];
+		console.log(firstNumber, lastNumber);
 		const number = Number(`${firstNumber}${lastNumber}`);
 		numbers.push(number);
 	}
 
 	const sum = numbers.reduce((a, b) => a + b, 0);
 	console.log(sum);
+
+	return sum;
 }
 
 main();
